73da225e7bb57980a25fa8970d6d2d893fd75139
nishant khandelwal <nishantkhandelwal69@gmail.com>
2017-10-11 00:12:08 +0530

    Adding basic configuration

--- src/App.js
+++ src/App.js
@@ -1,147 +1,157 @@
-import React from 'react'
-import { Dropdown } from 'semantic-ui-react'
-
-import { countryOptions } from './common';
-// countryOptions = [ { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' }, ...  ]
-
-// const DropdownExampleSearchSelection = () => (
-//   console.log("countryOptions",countryOptions),
-//   <Dropdown 
-//   placeholder='Select Country'
-//   fluid search selection
-//   options={countryOptions} />
-// )
-
-class DropdownExampleSearchSelection extends React.Component{
-
-constructor(props){
-  super(props);
-  this.state = {
-    loading:false,
-  }
-
-}
-  componentDidMount() {
-        fetch('http://dev.impactrun.com/api/team/?impactleague=9', {
-          method: 'GET',
-        })
-          .then(response => (response.json()))
-          .then((response) => {
-            this.setState({
-              teamObject: response,
-              loading:true
-            })
-            console.log("Teams", this.state.teamObject);
-          }).catch((err) => {
-            //console.log("err", err);
-          })
-      }
-  render(){
-    if(this.state.loading){
-      var option = JSON.stringify(this.state.teamObject);
-      console.log(option);
-      return(
-        <Dropdown 
-        placeholder='Select Country'
-        fluid search selection
-        options={option} />
-      );
-    }
-    else{
-      return(
-        <h1>Loading</h1>
-      )
-    }
-
-  }
-}
-export default DropdownExampleSearchSelection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './css/new-age.css';
+import Nav from './components/Nav.js';
+import Header from './components/Header.js'
 // import Clipboard from 'clipboard';
 // import Copy from './images/copytoclipboard.svg';
 // import 'primer-tooltips/index.scss';
 // import { Tooltip, Overlay, Button } from 'react-bootstrap';
+// import ReactTooltip from 'react-tooltip';
 
-// var clipboard = new Clipboard('.btn', {
-//   text: function (trigger) {
-//     return trigger.getAttribute('aria-label');
-//   }
-// });
-// clipboard.on('success', function (e) {
-//   console.log(e);
-// });
-// clipboard.on('error', function (e) {
-//   console.log(e);
-// });
-// const tooltip = (
-//   <Tooltip id="tooltip">Copy the code</Tooltip>
-// );
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
+class App extends React.Component {
 
-//     this.handleClick = e => {
-//       this.setState({ target: e.target, show: !this.state.show });
-//     };
+  render() {
 
-//     this.state = {
-//       show: false,
-//       teamObject: null
-//     };
-//   }
-//   componentDidMount() {
-//     fetch('http://dev.impactrun.com/api/team/?impactleague=9', {
-//       method: 'GET',
-//     })
-//       .then(response => (response.json()))
-//       .then((response) => {
-//         this.setState({
-//           teamObject: response,
-//         })
-//         console.log("Teams", this.state.teamObject);
-//       }).catch((err) => {
-//         //console.log("err", err);
-//       })
-//   }
+    return (
+      <div>
 
-//   render() {
-    
-//     return (
-//       <div className="App">
+        <Nav/>
 
-//         <input id="foo" value="https://github.com/zenorocha/clipboard.js.git" />
+       <Header/>
 
-//         <Overlay show={this.state.show} target={this.state.target} trigger="focus" placement="bottom" overlay={tooltip}>
+        <section className="download bg-primary text-center" id="download">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 mx-auto">
+                <h2 className="section-heading">Discover what all the buzz is about!</h2>
+                <p>Our app is available on any mobile device! Download now to get started!</p>
+                <div className="badges">
+                  <a className="badge-link" href="#"><img src="img/google-play-badge.svg" alt="" /></a>
+                  <a className="badge-link" href="#"><img src="img/app-store-badge.svg" alt="" /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
+        <section className="features" id="features">
+          <div className="container">
+            <div className="section-heading text-center">
+              <h2>Unlimited Features, Unlimited Fun</h2>
+              <p className="text-muted">Check out what you can do with this app theme!</p>
+              <hr />
+            </div>
+            <div className="row">
+              <div className="col-lg-4 my-auto">
+                <div className="device-container">
+                  <div className="device-mockup iphone6_plus portrait white">
+                    <div className="device">
+                      <div className="screen">
 
-//         </Overlay>
-//         <button className="btn" onClick={this.handleClick} aria-label="Copied" data-clipboard-target="#foo" >
-//           <img src={Copy} alt="Copy to clipboard" style={{ width: "50px" }} />
-//         </button>
-//       </div>
-//     );
-//   }
-// }
+                        <img src="img/demo-screen-1.jpg" className="img-fluid" alt="" />
+                      </div>
+                      <div className="button">
 
-// export default App;
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-8 my-auto">
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <div className="feature-item">
+                        <i className="icon-screen-smartphone text-primary"></i>
+                        <h3>Device Mockups</h3>
+                        <p className="text-muted">Ready to use HTML/CSS device mockups, no Photoshop required!</p>
+                      </div>
+                    </div>
+                    <div className="col-lg-6">
+                      <div className="feature-item">
+                        <i className="icon-camera text-primary"></i>
+                        <h3>Flexible Use</h3>
+                        <p className="text-muted">Put an image, video, animation, or anything else in the screen!</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <div className="feature-item">
+                        <i className="icon-present text-primary"></i>
+                        <h3>Free to Use</h3>
+                        <p className="text-muted">As always, this theme is free to download and use for any purpose!</p>
+                      </div>
+                    </div>
+                    <div className="col-lg-6">
+                      <div className="feature-item">
+                        <i className="icon-lock-open text-primary"></i>
+                        <h3>Open Source</h3>
+                        <p className="text-muted">Since this theme is MIT licensed, you can use it commercially!</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="cta">
+          <div className="cta-content">
+            <div className="container">
+              <h2>Stop waiting.<br />Start building.</h2>
+              <a href="#contact" className="btn btn-outline btn-xl js-scroll-trigger">Let's Get Started!</a>
+            </div>
+          </div>
+          <div className="overlay"></div>
+        </section>
+
+        <section className="contact bg-primary" id="contact">
+          <div className="container">
+            <h2>We
+          <i className="fa fa-heart"></i>
+              new friends!</h2>
+            <ul className="list-inline list-social">
+              <li className="list-inline-item social-twitter">
+                <a href="#">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li className="list-inline-item social-facebook">
+                <a href="#">
+                  <i className="fa fa-facebook"></i>
+                </a>
+              </li>
+              <li className="list-inline-item social-google-plus">
+                <a href="#">
+                  <i className="fa fa-google-plus"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <footer>
+          <div className="container">
+            <p>&copy; 2017 Start Bootstrap. All Rights Reserved.</p>
+            <ul className="list-inline">
+              <li className="list-inline-item">
+                <a href="#">Privacy</a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#">Terms</a>
+              </li>
+              <li className="list-inline-item">
+                <a href="#">FAQ</a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default App;
