b60bfb811599262e00fb3ae3320b693e53a7de20
nishant khandelwal <nishantkhandelwal69@gmail.com>
2017-11-05 15:15:21 +0530

    Added download pp icon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,11 +3,8 @@ import Contact_logo from "../img/contact-logo.png";
 const Contact = () => {
     return (
         <section className="contact grad-section" style={{ display: "flex" }} id="contact">
-
             <div style={{ width: "100%" }}>
                 <div className="col-sm-4 col-xs-12" style={{ display: "flex" }}>
-
-
                     <div className=" col-sm-6 ">
                         <img src={Contact_logo} alt="logo-contact" style={{ width: "106px" }} />
                     </div>
@@ -15,7 +12,6 @@ const Contact = () => {
                         <ul>
                             <li>
                                 <a href="#page-top">Home</a>
-
                             </li>
                             <li>
                                 <a href="#download">How&nbsp;It&nbsp;works?</a>
@@ -25,22 +21,32 @@ const Contact = () => {
                             </li>
                         </ul>
                     </div>
-
-
-
                 </div>
 
-                <div className="col-sm-4 col-xs-12 contact-m" >
-
-                    <ul style={{ WebkitPaddingStart: "0px" }}>
-                       
-                        <li>
-                        <a href="/privacy.html" target="_blank">Privacy policy</a>
+                <div className="col-sm-4 col-xs-12" style={{ display: "flex" }}>
+                    <div className="col-sm-6">
+                        <ul className="contact-last" style={{ WebkitPaddingStart: "10px" }}>
+                            <li>
+                                <a href="/privacy.html" target="_blank">Privacy policy</a>
                             </li>
-                        <li>
-                            <a href="/guideline.html" target="_blank">Guidelines</a>
-                        </li>
-                    </ul>
+                            <li>
+                                <a href="/guideline.html" target="_blank">Guidelines</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-sm-6">
+                    
+                            <p style={{textAlign:"center",marginBottom:"1px",padding:"2px"}}>Download App</p>
+                        
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            <a className="nav-link js-scroll-trigger" href="https://goo.gl/WM27CA" target="_blank" style={{ marginRight: "5px" }}>
+                                <i className="fa fa-android" aria-hidden="true" style={{ color: "black" }}></i>
+                            </a>
+                            <a className="nav-link js-scroll-trigger" target="_blank" href="https://goo.gl/qJPjzb">
+                                <i className="fa fa-apple" aria-hidden="true" style={{ color: "black" }}></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
                 <div className="col-sm-4 col-xs-12 contact-last">
                     <div>
